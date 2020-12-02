d500fce319898bda1015aa57a43ffae4aa4e5d01
wmh-git <1422308235@qq.com>
2020-12-02 14:59:57 +0800

    第一次完成完整的项目下载

---
+++ src/main/java/com/zhuoding/Git/Hello.java
@@ -0,0 +1,8 @@
+package com.zhuoding.Git;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("111111111");
+        System.out.println("难搞哦");
+    }
+}
